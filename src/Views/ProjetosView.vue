df63c15bb5e1b51c5b444077061d33fffb60fcaa
Angélica Anjos <angellica.anjos@hotmail.com>
2025-01-23 17:02:18 -0300

    Ajuste em projeto

--- src/Views/ProjetosView.vue
+++ src/Views/ProjetosView.vue
@@ -20,7 +20,7 @@ const projects = [
     {
         id: 1,
         title: 'Dashboard',
-        description: 'Projeto simula um dashboard de vendas, desenvolvido com Vue.js, HTML, CSS, Bootstrap e Chart.js. A aplicação exibe gráficos interativos e responsivos, utilizando dados estáticos para apresentar informações de vendas de forma visual e dinâmica.',
+        description: 'Projeto que simula um dashboard de vendas, desenvolvido com Vue.js, HTML, CSS, Bootstrap e Chart.js. A aplicação exibe gráficos interativos e responsivos, utilizando dados estáticos para apresentar informações de vendas de forma visual e dinâmica.',
         image: '/img/projeto1.png',
         viewLink: 'https://dashboardvenda.netlify.app/',
         repoLink: 'https://github.com/Angellica06/Dashboard',
@@ -28,18 +28,18 @@ const projects = [
     {
         id: 2,
         title: 'SaborChef',
-        description: 'Projeto de landing page de restaurante, desenvolvida com Angular, Html, Css e Boostrap. O objetivo principal da página é oferecer uma interface atraente e funcional, com informações sobre o restaurante, o menu e uma área de contato.',
+        description: 'Projeto de landing page de restaurante, desenvolvida com Angular, HTML, CSS e Bootstrap. O objetivo principal da página é oferecer uma interface atraente e funcional, com informações sobre o restaurante, o menu e uma área de contato.',
         image: '/img/projeto2.png',
         viewLink: 'https://saborchef.netlify.app/',
         repoLink: 'https://github.com/Angellica06/SaborChef',
     },
     {
         id: 3,
-        title: 'Lista de Compras',
-        description: 'Projeto de lista de compras, desenvolvido com Angular, Html, Css e Bootstrap, com as funcionalidades de logar a partir de uma conta do Google, adicionar item a lista, podendo marcar como comprado ou não comprado, além de poder editar e remover o item.',
+        title: 'Calculadora IMC',
+        description: 'Uma calculadora de IMC (Índice de Massa Corporal) criada com HTML, CSS e JavaScript. A aplicação permite inserir o peso e altura do usuário e exibe sua classificação, ajudando a acompanhar a saúde de forma prática. O design é simples e funcional.',
         image: '/img/projeto3.png',
-        viewLink: 'https://lista-de-compras1.netlify.app',
-        repoLink: 'https://github.com/Angellica06/Lista-de-compras',
+        viewLink: 'https://calculadora-imc13.netlify.app/',
+        repoLink: 'https://github.com/Angellica06/Calculadora_IMC?tab=readme-ov-file',
     }
 ];
 </script>
